776f055255d63ef8c4cb6c30961174c0c5cb2454
Andreas Octavianus <andreas.octavianus@gmail.com>
2018-01-09 22:40:42 +0700

    current version of authorization server

--- authorization-server/src/main/java/com/demo/authorizationserver/AuthorizationServerApplication.java
+++ authorization-server/src/main/java/com/demo/authorizationserver/AuthorizationServerApplication.java
@@ -2,8 +2,11 @@ package com.demo.authorizationserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
+@EnableResourceServer
 public class AuthorizationServerApplication {
 
 	public static void main(String[] args) {
